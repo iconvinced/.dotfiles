4ade68ce575cebbc9ba40ce7c79ed762fc39ce1f
Plenty Su <plentysu@wherever>
2013-08-24 16:46:42 +0800

    Add bootstrap.sh
    * script to install all .dotfiles to specified folder

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $# -ne "1" ]]; then
+    echo "$0 <folder to install>"
+    exit -1
+fi
+
+INSTALL_FOLDER=$1
+
+INSTALL_FILES=(
+    ".bash_profile"
+    ".gitconfig"
+    ".vimrc"
+)
+
+echo "install .dotfiles to $INSTALL_FOLDER:"
+if [[ -d $INSTALL_FOLDER ]]; then
+    for file in ${INSTALL_FILES[@]}; do
+        echo "    $file"
+        cp $file $INSTALL_FOLDER
+    done
+fi
+echo "done"
