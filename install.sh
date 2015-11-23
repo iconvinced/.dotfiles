8de80587a56272e812a1066fede05dcc4dc23a3d
Plenty Su <plenty@wherever>
2015-11-24 02:00:39 +0800

    Fix vundle preinstall path

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ pre_install()
 {
     cd $IN
     rm -rf .vim/
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 }
 
 run()
